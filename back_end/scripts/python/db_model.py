bf228c1d94c0d7a41dbda1c2c5550b6a64e3179e
kentrosewi <kentrosewi@gmail.com>
2018-07-10 18:00:31 -0500

    Adding db model foreign keys + relationships

--- back_end/scripts/python/db_model.py
+++ back_end/scripts/python/db_model.py
@@ -8,7 +8,8 @@ db = db_config.db
 class Client(db.Model):
 	__tablename__ = 'client'
 	id = db.Column('id', db.Integer, primary_key=True)
-	name = db.Column('name', db.Unicode)
+	name = db.Column('name', db.Unicode, nullable=False)
+	feature_requests = db.relationship('FeatureRequest', backref='client', lazy=True)
 	def __init__(self, id, name):
 		self.id = id
 		self.name = name
@@ -16,7 +17,8 @@ class Client(db.Model):
 class ProductArea(db.Model):
 	__tablename__ = "product_area"
 	id = db.Column('id', db.Integer, primary_key=True)
-	name = db.Column('name', db.Unicode)
+	name = db.Column('name', db.Unicode, nullable=False)
+	feature_requests = db.relationship('FeatureRequest', backref='product_area', lazy=True)
 	def __init__(self, id, name):
 		self.id = id
 		self.name = name
@@ -24,12 +26,12 @@ class ProductArea(db.Model):
 class FeatureRequest(db.Model):
 	__tablename__ = "feature_request"
 	id = db.Column('id', db.Integer, primary_key=True)
-	title = db.Column('title', db.Unicode)
-	description = db.Column('description', db.Unicode)
-	client_id = db.Column('client_id', db.Integer)
-	priority = db.Column('priority', db.Integer)
-	target = db.Column('target', db.Date)
-	product_area_id = db.Column('product_area_id', db.Integer)
+	title = db.Column('title', db.Unicode, nullable=False)
+	description = db.Column('description', db.Unicode, nullable=False)
+	client_id = db.Column('client_id', db.Integer, db.ForeignKey('client.id'), nullable=False)
+	priority = db.Column('priority', db.Integer, nullable=False)
+	target = db.Column('target', db.Date, nullable=False)
+	product_area_id = db.Column('product_area_id', db.Integer, db.ForeignKey('product_area.id'), nullable=False)
 	def __init__(self, id, title, description, client_id, priority, target, product_area_id):
 		self.id = id
 		self.title = title
