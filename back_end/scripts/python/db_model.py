d87ec2bdaf23812dd9cf045fd9b08fae412df355
kentrosewi <kentrosewi@gmail.com>
2018-07-09 21:53:57 -0500

    Cleanup.

--- back_end/scripts/python/db_model.py
+++ back_end/scripts/python/db_model.py
@@ -1,12 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-#import db_config
+
 import db_config
 
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = db_config.db_uri
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
 db = db_config.db
 
 class Client(db.Model):
